be6ccedc20819568475102fd4072dd50008188c2
temtomk <temtomk@gmail.com>
2024-01-14 23:12:21 +0900

    [FIX] Change the wallet button to be smaller

--- app/_component/wallet-connect.jsx
+++ app/_component/wallet-connect.jsx
@@ -9,10 +9,10 @@ export default function WalletConnect() {
   return (
     <div className="flex justify-end margin-right">
       <button
-        className="text-white text-center text-sm sm:text-base md:text-[2vw] whitespace-nowrap justify-center items-stretch mr-2 sm:mr-11 h-10 sm:h-12 md:h-14 lg:h-16 xl:h-20 px-2 sm:px-8 py-1 sm:py-4 rounded-lg border-2 border-solid border-white self-end max-sm:mr-2.5 max-sm:px-5"
+        className="text-white text-center text-sm sm:text-base md:text-[2vw] whitespace-nowrap justify-center items-stretch mr-2 sm:mr-11 h-8 sm:h-10 md:h-12 lg:h-14 xl:h-18 px-1 sm:px-6 py-1 sm:py-3 rounded-lg border-2 border-solid border-white self-end max-sm:mr-2.5 max-sm:px-4"
         onClick={connectWallet}
       >
-        {isConnected ? "Wallet Connected" : "Connect Wallet"}
+        {isConnected ? "Disconnect" : "Connect"}
       </button>
     </div>
   );

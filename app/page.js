762042a53733d67592b5ba2476228324dcd8a9a5
temtomk <temtomk@gmail.com>
2024-01-28 13:57:42 +0900

    feat: ✨ Remove actions from wallet and add disagree rate display

--- app/page.js
+++ app/page.js
@@ -5,13 +5,14 @@ import Pdtx from "../public/pdtx.svg";
 import Total from "./components/total";
 import Main from "./components/main";
 import Blocks from "./components/blocks";
+import { Disagree } from "./components/disagree";
 // import ProfileTotal from "./component/profile-total";
 
 export default function Component() {
   return (
     <WalletProvider>
       <div className="bg-zinc-800 flex flex-col py-11">
-        <WalletConnect />
+        {/* <WalletConnect /> */}
         <div className="flex items-center justify-center text-white text-center text-6xl tracking-[7px] self-center whitespace-nowrap mt-20 max-md:text-4xl max-md:mt-5">
           <Pdtx width="1em" height="1em" />
           <span className="font-bold ml-1">$PDTX (NO PDT)</span>
@@ -22,30 +23,26 @@ export default function Component() {
           누구를 위한 합병인가, 전면백지화 혹은 비율 재산정하라!
           <br />
           <br />
-          부결 행동강령입니다.
+          <Disagree />
           <br />
           <div className="font-bold">
-            기본지식: 거버넌스 투표 스냅샷은 2/2 투표종료시 입니다.
+            기본지식 1) 2/2 투표종료시 거버넌스 투표 스냅샷 입니다.
             <br />
-            네오핀 자체투표 스냅샷은 1/26 오전 8시 50분입니다.
+            기본지식 2) 강력반대 33% 이상, 혹은 반대 51% 이상일 시 부결입니다.
             <br />
             <br />
-            1) 아직 스테이킹 안한 경우: 1/26 오전 7시 30분전까지 네오핀
-            자체지갑에 예치 후 직접 투표.{" "}
-            <span className="text-yellow-300">(마감)</span>
-            <br />
-            2) 언스테이킹 하고자 한다면: 언스테이킹 끝나는대로 2/1 자정까지
-            네오핀벨리에 예치.
-            <br />
-            다만, 1/25 자정까지 언스테이킹 완료 필요.{" "}
-            <span className="text-yellow-300">(마감)</span>
-            <br />
-            3) 재위임 하고자 한다면: 재위임 락 끝나는대로 네오핀 재위임.
           </div>
+          현재 강력반대를 위해 거버넌스 투표 스냅샷 전까지{" "}
+          <span className="text-yellow-300 font-bold">버그홀 </span>
+          또는 <span className="text-yellow-300 font-bold">A41 </span>에
+          투표권을 위임해야 합니다.
           <br />
-          위 내용과 별개로 합병 반대운동 진행 중입니다.
+          <br />
+          1차 합병 제안 부결 시 $PDTX 홀더들 대상으로 치킨 3마리 에어드랍
+          하겠습니다.
           <br />
           <br />
+          <div className="font-bold">[$PDTX 채굴하는 방법]</div>
           자기 자신에게 FNSA를 송금하여 MEMO 란에
           <br />
           <p className="font-bold text-yellow-300">
@@ -54,8 +51,6 @@ export default function Component() {
           라고 입력 후 전송하시면 1$PDTX 채굴 완료!
           <br />
           <br />
-          합병백지화 혹은 비율 재산정 시 $PDTX 홀더들 대상으로 치킨 에어드랍
-          하겠습니다.
         </div>
 
         <Total />

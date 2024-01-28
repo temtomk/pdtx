e9ae4fa40777982418bc7de4b98234050c18fcd0
temtomk <temtomk@gmail.com>
2024-01-28 14:03:55 +0900

    feat: ✨ Remove actions from wallet and add disagree rate display

--- app/components/button-container/container.tsx
+++ app/components/button-container/container.tsx
@@ -16,17 +16,17 @@ const Container: React.FC<ContainerProps> = ({
   setShowBalance,
 }) => {
   return (
-    <div className="grid grid-cols-4 sm:grid-cols-4 xl:grid-cols-4 flex items-stretch self-stretch justify-between gap-5 mt-10 max-md:max-w-full max-md:flex-wrap max-md:mt-10 max-sm:flex max-sm:flex-col max-sm:self-stretch max-sm:items-center max-sm:justify-between max-sm:w-auto max-sm:h-auto max-sm:grow-0">
-      <div className="col-span-1 w-full">
+    <div className="flex items-stretch self-stretch justify-between gap-5 mt-10 max-md:max-w-full max-md:flex-wrap max-md:mt-10 max-sm:flex max-sm:flex-col max-sm:self-stretch max-sm:items-center max-sm:justify-between max-sm:w-auto max-sm:h-auto max-sm:grow-0">
+      {/* <div className="col-span-1 w-full">
         <Minting onButtonClick={checkConnection} />
-      </div>
-      <div className="col-span-1 w-full shrink-0 inline-block">
+      </div> */}
+      {/* <div className="col-span-1 w-full shrink-0 inline-block">
         <Profile
           onButtonClick={checkConnection}
           setBalance={setBalance}
           setShowBalance={setShowBalance}
         />
-      </div>
+      </div> */}
       <div className="col-span-1 w-full flex-col justify-center items-center">
         <Twitter />
       </div>
